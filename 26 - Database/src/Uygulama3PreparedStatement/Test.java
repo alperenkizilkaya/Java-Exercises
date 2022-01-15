5f813ac4aa11b318c34edbb99ea7f7597152d96b
Alperen Kizilkaya <alperen.kzlkaya@gmail.com>
2022-01-15 03:47:19 +0300

    java exercises

--- 26 - Database/src/Uygulama3PreparedStatement/Test.java
+++ 26 - Database/src/Uygulama3PreparedStatement/Test.java
@@ -1,16 +1,5 @@
 package Uygulama3PreparedStatement;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
-
 public class Test {
 
     public static void main(String[] args) {
@@ -27,11 +16,11 @@ public class Test {
 
         try {
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "alperen123");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root","alperen123");
 
             System.out.println("başarılı şekilde db bağlantısı sağlandı..");
 
-            String query = "CREATE TABLE Ogrenci (" +
+            String query = "CREATE TABLE deneme (" +
                     "           ogrenciID INT NOT NULL," +
                     "           adi VARCHAR(20) NOT NULL," +
                     "           soyadi VARCHAR(20) NOT NULL," +
@@ -45,14 +34,13 @@ public class Test {
         } catch (Exception e) {
             System.err.println("hata : " + e);
         } finally {
-            if (preparedStatement != null) {
+            if(preparedStatement != null) {
                 try {
                     preparedStatement.close();
                 } catch (SQLException e) {
                     System.err.println("statement kapanırken hataaa ");
                 }
-            }
-            if (connection != null) {
+            }if(connection != null){
                 try {
                     connection.close();
                 } catch (SQLException e) {
